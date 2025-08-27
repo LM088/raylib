66e1f85f0c9975be22eb594b0a3f6a693be71c01
LM088 <69414981+LM088@users.noreply.github.com>
2025-08-27 10:44:57 +0000

    Add paddle class

--- pong.cpp
+++ pong.cpp
@@ -30,7 +30,46 @@ public:
     }
 };
 
+
+class Paddle 
+{
+public:
+    float x,y = 0;
+    float width, height = 0;    
+    int speed= 0; 
+
+    void Draw()
+    {
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+
+    void Update()
+    {
+
+        if (IsKeyDown(KEY_UP))
+        {
+            y -= speed; 
+        }
+
+        if (IsKeyDown(KEY_DOWN))
+        {
+            y += speed;
+        }
+
+        if ( y + height >= GetScreenHeight())
+        {
+            y = (GetScreenHeight()-height) - 10 ;  
+        }
+
+        if (y  <= 0)
+        {
+            y = 10; 
+        }
+    }
+};
+
 Ball ball; 
+Paddle player; 
 
 int main()
 {
@@ -40,11 +79,18 @@ int main()
     InitWindow(screen_width, screen_height, "Pong!");
     SetTargetFPS(60);
 
+
     ball.x= screen_width/2;
     ball.y= screen_height/2;
     ball.radius= 20;
     ball.speed_x= 7;
     ball.speed_y= 7;
+
+    player.width= 25;
+    player.height= 120;
+    player.x= 10; 
+    player.y= screen_height/2 - player.height/2;
+    player.speed= 6;
 
     
     while(!WindowShouldClose())
@@ -53,12 +99,13 @@ int main()
 
         // Updating position
         ball.Update();
+        player.Update();
 
         // clear background before drawing 
         ClearBackground(BLACK);
         DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
         ball.Draw();
-        DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);
+        player.Draw();
         // -35 and not -10 because you have to take rectangle width into consideration. 
         // rectangle is drawn from TOP LEFT corner 
         DrawRectangle(screen_width-35, screen_height/2-60, 25, 120, WHITE);
