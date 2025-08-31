1c9bbacb7aa1d8bd1dab14f24f4e56f13683b74c
LM088 <69414981+LM088@users.noreply.github.com>
2025-08-31 05:09:31 +0000

    Make snake movement unidirectional.

--- snake.cpp
+++ snake.cpp
@@ -129,6 +129,23 @@ int main()
             snake.Update();
         }
         
+        if (IsKeyPressed(KEY_DOWN) && snake.direction.y != -1)
+        {
+            snake.direction= {0,1};
+        }
+        if (IsKeyPressed(KEY_UP) && snake.direction.y != 1)
+        {
+            snake.direction= {0,-1};
+        }
+        if (IsKeyPressed(KEY_LEFT) && snake.direction.x != 1)
+        {
+            snake.direction= {-1,0};
+        }
+        if (IsKeyPressed(KEY_RIGHT) && snake.direction.x != -1)
+        {
+            snake.direction= {1,0};
+        }
+        
 
         // Draw
         ClearBackground(green);
